7dd9834e253c71cea30fb31546a01e6d76e12375
audinefroberto-max <audinefroberto@gmail.com>
2026-06-11 11:29:28 -0300

    Update ban.lua

--- ban.lua
+++ ban.lua
@@ -1,1 +1,13 @@
+-- =========================================================================
+-- LISTE NOIRE DES COMPTES BANNIS
+-- =========================================================================
+-- Ce script est la base de données. Il doit être hébergé sur GitHub.
+-- Le script principal viendra lire ces informations pour bloquer les joueurs.
 
+local ListeNoire = {
+    [123456789] = true,
+}
+
+-- Ne modifie pas cette partie. Elle permet au script principal 
+-- de récupérer les informations de cette liste correctement.
+return ListeNoire
